2b3c68e78c8344b9fb80e0e98edecd0ba0852c79
Evellyn Aparecida Ferreira dos Santos <evellynaparecida0@gmail.com>
2024-10-07 16:29:58 -0300

    fix: fiz o codigo do MetaController, usei o php forja criar model Meta me retornou que O arquivo Meta já existe sem existir

--- app/Controllers/MetaController.php
+++ app/Controllers/MetaController.php
@@ -3,15 +3,27 @@
 namespace App\Controllers;
 
 use Hefestos\Core\Controller;
+use App\Models\MetaModel;
 
 class MetaController extends Controller
 {
+    protected $meta_model;
+
     public function index()
     {
-        // return view();
-        // ou
-        // return json();
-        // ou ainda
-        // return redirecionar(); 
+        $metas = $this->meta_model->todos();
+
+        return view('metas/metas', [
+            'metas' => $metas
+        ]);
+         
+    }
+
+    public function create(){
+        return view('metas/novo');
+    }
+
+    public function store(){
+        return redirecionar('/');
     }
 }
