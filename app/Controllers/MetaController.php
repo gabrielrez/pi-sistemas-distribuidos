22e28bf400967000923d40df53a9ffe07410cc30
EvellynSantos17 <evellynaparecida0@gmail.com>
2024-10-20 18:59:59 -0300

    MetaController terminado

--- app/Controllers/MetaController.php
+++ app/Controllers/MetaController.php
@@ -3,15 +3,35 @@
 namespace App\Controllers;
 
 use Hefestos\Core\Controller;
+use App\Models\Meta;
 
 class MetaController extends Controller
 {
+    protected $meta_model;
+
+    public function __construct()
+    {
+        $this->meta_model = new Meta();
+    }
+
     public function index()
     {
-        // return view();
-        // ou
-        // return json();
-        // ou ainda
-        // return redirecionar(); 
+        $metas = $this->meta_model->where(['id_usuario' => sessao()->pegar('usuario.id')])->todos();
+
+        return view('metas/metas', [
+            'metas' => $metas
+        ]);
+    }
+
+    public function create()
+    {
+        return view('metas/novo');
+    }
+
+    public function store()
+    {
+        $meta = $this->dadosPost();
+        $this->meta_model->insert($meta);
+        return redirecionar('/dashboard');
     }
 }
