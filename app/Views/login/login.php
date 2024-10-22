a9626e546cb261b7758b108739fced03574b590d
YasmimSS <162493222+YasmimSS@users.noreply.github.com>
2024-10-22 00:58:33 +0000

    login

---
+++ app/Views/login/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    
+    <div class="fotologin"> 
+        <h1>
+            
+        </h1>
+
+        <div class="quadradocor">
+            <h1>Suas finanças agradecem!</h1>
+        </div>
+    </div>
+
+    <div class="loginesquerda">
+        <form action="" class="login">
+
+            <div class="logo"></div>
+            
+
+            <div class="inputs">
+                <input type="text" placeholder="E-mail">
+                <br>
+                <input type="text"placeholder="Senha">
+                <br><br>
+                <a href="#">Esqueceu a senha?</a>
+                <a href="Cadastro.html">Criar conta</a>
+               
+                
+            </div>
+            
+        </form>
+    </div>
+
+    
+</body>
+</html>
