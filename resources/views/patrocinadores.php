1c680ee94939b5334db4772e0ecfb17eba3ae837
Giullian_Bernard <129344709+Giullianbernard@users.noreply.github.com>
2024-10-28 14:14:38 -0300

    Create patrocinadores.php

---
+++ resources/views/patrocinadores.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+//achar as img dos patrocinadores
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patrocinadores</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="sponsor-section">
+        <h1>Nossos Patrocinadores</h1>
+        <div class="sponsor-grid">
+            <div class="sponsor-item">
+                <img src="logo1.png" alt="Patrocinador 1">
+            </div>
+            <div class="sponsor-item">
+                <img src="logo2.png" alt="Patrocinador 2">
+            </div>
+            <div class="sponsor-item">
+                <img src="logo3.png" alt="Patrocinador 3">
+            </div>
+            <div class="sponsor-item">
+                <img src="logo4.png" alt="Patrocinador 4">
+            </div>
+            <div class="sponsor-item">
+                <img src="logo5.png" alt="Patrocinador 5">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
