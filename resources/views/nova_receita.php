baae53b8dad27a7cfff1756face98c947ad088df
Giullian_Bernard <129344709+Giullianbernard@users.noreply.github.com>
2024-10-28 13:53:26 -0300

    Create nova_receita.php

---
+++ resources/views/nova_receita.php
@@ -0,0 +1,37 @@
+<h1>Nova Receita</h1>
+
+<a href="/">Voltar</a>
+<br>
+<br>
+
+<form action="/receitas" method="POST">
+    <div class="input">
+        <label for="categoria">Categoria</label>
+        <select name="categoria" id="categoria_select">
+            <option value="salario">Salário</option>
+            <option value="freelance">Freelance</option>
+            <option value="aluguel">Aluguel</option>
+            <option value="investimento">Investimento</option>
+            <option value="pensao">Pensão</option>
+            <option value="premio-loteria">Prêmio/Loteria</option>
+            <option value="aposentadoria">Aposentadoria</option>
+            <option value="heranca">Herança</option>
+            <option value="venda-de-bens">Venda de bens</option>
+            <option value="comissao">Comissão</option>
+            <option value="emprestimo">Empréstimo</option>
+            <option value="doacao">Doação</option>
+            <option value="reembolso">Reembolso</option>
+            <option value="outros">Outros</option>
+        </select>
+    </div>
+    <div class="input">
+        <label for="valor">Valor</label>
+        <input type="number" name="valor" placeholder="00.00 R$">
+    </div>
+    <div class="input">
+        <label for="descricao">Descrição</label>
+        <textarea name="descricao" id="descricao_text_area"></textarea>
+    </div>
+    <input type="hidden" name="id_usuario" value="<?= $_SESSION['id'] ?>">
+    <button>Adicionar</button>
+</form>
