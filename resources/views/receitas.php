2b3f4fa33ec6ccb2512fe043efdef08b23095623
Giullian_Bernard <129344709+Giullianbernard@users.noreply.github.com>
2024-10-28 13:54:01 -0300

    Create receitas.php

---
+++ resources/views/receitas.php
@@ -0,0 +1,21 @@
+<h1>Receitas</h1>
+
+<a href="/">Voltar</a>
+<br>
+<br>
+<a href="/receitas/novo">Adicionar Receita</a>
+
+<br>
+<br>
+
+<?php
+
+foreach ($receitas as $receita) {
+    echo '<ul>';
+    echo '<li>' . 'Tipo: ' . $receita['categoria'] . '</li>';
+    echo '<li>' . 'Descrição: ' . $receita['descricao'] . '</li>';
+    echo '<li>' . 'Valor: ' . $receita['valor'] .  ' R$' . '</li>';
+    echo '</ul>';
+}
+
+?>
