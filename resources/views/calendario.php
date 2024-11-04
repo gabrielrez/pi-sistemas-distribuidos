03c0f37e09db2d7731a534098a6a76b900989ac9
Giullian_Bernard <129344709+Giullianbernard@users.noreply.github.com>
2024-11-04 18:46:54 -0300

    calendario.php

---
+++ resources/views/calendario.php
@@ -0,0 +1,57 @@
+<div class="calendar">
+    <div class="header">
+        <h2></h2>
+    </div>
+    <div class="dia">
+        <div class="day"><strong>Dom</strong></div>
+        <div class="day"><strong>Seg</strong></div>
+        <div class="day"><strong>Ter</strong></div>
+        <div class="day"><strong>Qua</strong></div>
+        <div class="day"><strong>Qui</strong></div>
+        <div class="day"><strong>Sex</strong></div>
+        <div class="day"><strong>Sab</strong></div>
+        
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+        <div class="day empty"></div>
+
+        <div class="day">1</div>
+        <div class="day">2</div>
+        <div class="day">3</div>
+        <div class="day">4</div>
+        <div class="day">5</div>
+        <div class="day">6</div>
+        <div class="day">7</div>
+        <div class="day">8</div>
+        <div class="day">9</div>
+        <div class="day">10</div>
+        <div class="day">11</div>
+        <div class="day">12</div>
+        <div class="day">13</div>
+        <div class="day">14</div>
+        <div class="day">15</div>
+        <div class="day">16</div>
+        <div class="day">17</div>
+        <div class="day">18</div>
+        <div class="day">19</div>
+        <div class="day">20</div>
+        <div class="day">21</div>
+        <div class="day">22</div>
+        <div class="day">23</div>
+        <div class="day">24</div>
+        <div class="day">25</div>
+        <div class="day">26</div>
+        <div class="day">27</div>
+        <div class="day">28</div>
+        <div class="day">29</div>
+        <div class="day">30</div>
+        <div class="day">31</div>
+    </div>
+</div>
+
+</body>
+</html>
